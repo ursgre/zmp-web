4f98ccc15758259e16180e0d0f49d12b22847352
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2023-05-16 22:16:45 +0200

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
         {users.length > 0 && <div className="user">
           {users.map(user=>(
          <div className="container"> 
-          <p style={{padding: "0.25rem", borderBottom: "1px solid white"}} onClick={() => navigate(`/profile/${user.id}`) }>{user.username}</p>
+          <p style={{padding: "0.25rem", borderBottom: "1px solid white"}} onClick={() => {setSearch(""); setUsers([]); navigate(`/profile/${user.id}`)} }>{user.username}</p>
          </div>
           ))}
         </div>}
